f2cac5f88f5583d24f434eb818d1f6c4dc7c7600
jnjohnny <19-39366-1@student.aiub.edu>
2021-03-02 00:18:42 +0600

    Account & Address Class update

--- lab1/Account.cs
+++ lab1/Account.cs
@@ -6,7 +6,107 @@ using System.Threading.Tasks;
 
 namespace lab1
 {
-    class Account
+    public class Account
     {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+        int a = 1;
+
+        public int AccountNumber
+        {
+            //set { this.accountName = value; }
+            get { return accountNumber; }
+        }
+
+        public string AccountName
+        {
+            set { this.accountName = value; }
+            get { return this.accountName; }
+        }
+        public double Balance
+        {
+            set { this.balance = value; }
+            get { return this.balance; }
+        }
+
+        public Address Address
+        {
+            set { this.address = value; }
+            get { return this.address; }
+        }
+
+        public void PrintAccount()
+        {
+
+            Console.WriteLine("\nAccount Name:{0}\nBalance:{1}", this.accountName, this.balance);
+            this.address.GetAddress();
+        }
+
+
+
+        public void print()
+        {
+            this.accountNumber = a;
+            Console.WriteLine("Your Account No is=" + this.accountNumber + "\n");
+            a++;
+        }
+
+
+
+        public void deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Console.WriteLine("Previous Balance: " + balance);
+                Console.WriteLine("Deposit Amount: " + amount);
+                balance += amount;
+                Console.WriteLine("Current Balance: " + balance);
+            }
+            else
+            {
+                Console.WriteLine("Can Not Deposit");
+            }
+        }
+
+
+
+        public void withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                Console.WriteLine("Previous Balance:	" + balance);
+                Console.WriteLine("Withdraw Amount:	" + amount);
+                balance -= amount;
+                //balance = balance - amount;
+                Console.WriteLine("Current Balance:	" + balance);
+            }
+            else
+            {
+                Console.WriteLine("Can Not Withdraw");
+            }
+        }
+
+
+
+
+
+        public void transfer(Account a, double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                Console.WriteLine("Previous Balance:	" + this.balance);
+                Console.WriteLine("Transfer Amount:	" + amount);
+                this.balance = this.balance - amount;
+                // a.balance = a.balance + amount;
+                Console.WriteLine("Current Balance:	" + this.balance);
+            }
+            else
+            {
+                Console.WriteLine("Can Not Transfer");
+            }
+        }
+
     }
 }
--- lab1/Address.cs
+++ lab1/Address.cs
@@ -8,5 +8,45 @@ namespace lab1
 {
     class Address
     {
+        private string roadNo;
+        private string houseNo;
+        private string city;
+        private string country;
+
+        public Address(string houseNo, string roadNo, string city, string country)
+        {
+            this.houseNo = houseNo;
+            this.roadNo = roadNo;
+            this.city = city;
+            this.country = country;
+        }
+        public string HouseNo
+        {
+            set { this.houseNo = value; }
+            get { return this.houseNo; }
+        }
+        public string RoadNo
+        {
+            set { this.roadNo = value; }
+            get { return this.roadNo; }
+        }
+
+        public string City
+        {
+            set { this.city = value; }
+            get { return this.city; }
+        }
+
+        public string Country
+        {
+            set { this.country = value; }
+            get { return this.country; }
+        }
+        public string GetAddress()
+        {
+            string x = "Road No=" + roadNo + "," + "House No=" + houseNo + "," + "City=" + city + "," + "Country=" + country;
+            Console.WriteLine("Address:House No-{0},Road No-{1},City-{2},Country-{3}", this.houseNo, this.roadNo, this.city, this.country);
+            return x;
+        }
     }
 }
